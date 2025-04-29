002968c3c0ed1fb291e4c6e3ea8e48c899081ac3
CiamalaKombeGreg <he202098@students.ephec.be>
2025-04-29 15:13:43 +0200

    Added jeton verification in Auth Backend

--- apps/app-back/src/auth/auth.module.ts
+++ apps/app-back/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from "src/prisma/prisma.module";
 import { AuthService } from "./auth.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ConfigModule.forRoot()],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/app-back/src/auth/auth.service.ts
+++ apps/app-back/src/auth/auth.service.ts
@@ -1,19 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TokenSchema } from '../schemas/auth';
+import { OAuth2Client } from 'google-auth-library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  async getAuthResponse(data: {
-    email: string;
-    token: string;
-    nom: string;
-  }) {
+  async getAuthResponse(data: { email: string; token: string; nom: string }) {
+    if (
+      process.env.GOOGLE_ANDROID_CLIENT === undefined ||
+      process.env.GOOGLE_IOS_CLIENT === undefined
+    ) {
+      return {
+        status: 500,
+        message: 'No google client provided in environment.',
+      };
+    }
+
+    const client = new OAuth2Client();
+
     try {
+      const ticket = await client.verifyIdToken({
+        idToken: data.token,
+        audience: [
+          process.env.GOOGLE_ANDROID_CLIENT,
+          process.env.GOOGLE_IOS_CLIENT,
+        ],
+      });
+      const payload = ticket.getPayload();
+
+      if (!payload) {
+        return {
+          status: 401,
+          message:
+            'Unauthorized with no payload received while verifying token.',
+        };
+      }
+
+      const userId = payload['sub'];
+
       const response = await fetch(
-        `https://oauth2.googleapis.com/tokeninfo?id_token=${data.token}`,
+        `https://oauth2.googleapis.com/tokeninfo?id_token=${userId}`,
       );
       const result = await response.json();
 
@@ -43,12 +71,21 @@ export class AuthService {
         });
 
         if (findPersonnel) {
-          return { status: '200' };
+          return {
+            status: '200',
+            message: 'This email is already in registered.',
+          };
         } else {
-          return { status: '200', data: result };
+          return {
+            status: '200',
+            message: 'This email has been added to the personnel.',
+          };
         }
       } else {
-        return { status: '401' };
+        return {
+          status: '500',
+          message: "Token infos doesn't match what is asked in.",
+        };
       }
     } catch (error) {
       return { status: '500', error: error };
