84580f0a1d6e895b0175f2adb94dc8ca641a66e6
CiamalaKombeGreg <he202098@students.ephec.be>
2025-06-03 02:01:24 +0200

    Added swagger

--- apps/app-back/src/main.ts
+++ apps/app-back/src/main.ts
@@ -1,9 +1,21 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
   console.log("Connected to API NestJs");
