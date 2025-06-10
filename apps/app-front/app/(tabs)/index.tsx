ac392db2b92f9a56324cfb2a639eec068925c3b9
CiamalaKombeGreg <he202098@students.ephec.be>
2025-06-10 08:09:53 +0200

    Added belgium holiday to accueil

--- apps/app-front/app/(tabs)/index.tsx
+++ apps/app-front/app/(tabs)/index.tsx
@@ -7,6 +7,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useState } from "react";
 import { AuthResponse } from "@/lib/types";
 import { useGetStatus } from "@/components/hooks/useGetStatus";
+import { useGetBelgiumHolidays } from "@/components/hooks/useGetBelgiumHolidays";
+import { toDateId } from "@marceloterreiro/flash-calendar";
+
+const today = new Date();
+const todayId = toDateId(today);
 
 const indexDays = {
   "1" : "Lundi",
@@ -34,12 +39,34 @@ const Index = () => {
 
   // Current date
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
+
+  // Holiday in belgium
+  const {data : belgiumHoliday, isLoading : currentLoading} = useGetBelgiumHolidays(`${new Date().getFullYear()}`);
+
+  const [currentBelgiumHoliday, setCurrentBelgiumHoliday] = useState<string>("")
+
+  // Update current date
+  const changeDate = () => {
+    if(Array.isArray(belgiumHoliday)){
+      for(const element of belgiumHoliday){
+        if(element.dateId === todayId){
+          setCurrentBelgiumHoliday(element.name);
+          return;
+        }
+      }
+    }else{
+      setCurrentBelgiumHoliday("Aucun congé");
+    }
+    setCurrentBelgiumHoliday("Aucun congé");
+  }
   
   // Current status
   const {data : conge, isLoading} = useGetStatus(userInfo?.user.email || "");
 
-  if(isLoading){
+  if(isLoading && currentLoading){
     return <SafeAreaView className="flex flex-col justify-content items-center"><ActivityIndicator /></SafeAreaView>
+  }else if(currentBelgiumHoliday === ""){
+    changeDate()
   }else{
     return (
       <SafeAreaView className="flex items-center w-full h-full">
@@ -51,7 +78,7 @@ const Index = () => {
             <Text className="text-cyan-700 font-bold text-5xl">{indexDays[currentDate.getDay() as 1 | 2 | 3 | 4 | 5 | 6 | 7]}</Text>
             {/* Status */}
             {Array.isArray(conge) && conge.length > 0 && (conge.length > 1 ? <Text>Attention : Deux congés se superposent</Text> : <Text className="text-cyan-700 text-xl">Congé actuel : {conge[0].title}</Text>)}
-            {Array.isArray(conge) && conge.length === 0 && <Text className="text-cyan-700 text-2xl">Aucun congé</Text>}
+            {Array.isArray(conge) && conge.length === 0 && <Text className="text-cyan-700 text-2xl">{currentBelgiumHoliday}</Text>}
           </View>
           {/* Liste of notifications (requests from the closest to the lastest without order of people) */}
       </SafeAreaView>
